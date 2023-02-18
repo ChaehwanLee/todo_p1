c6aea59e3eded4b10c93515e417ce9e0c9d78f56
Chaehwan Lee <oasisinyourmind@gmail.com>
2023-02-19 00:34:33 +0900

    Delete button

--- src/component/todoitem.js
+++ src/component/todoitem.js
@@ -1,5 +1,38 @@
 import React from "react";
 
 function Todoitem({id, fin, text}){
-    const []
+    const [id, setId]
 }
+
+
+// import React from 'react';
+
+// const DeleteButton = (props) => {
+//   return (
+//     <button onClick={props.onClick}>Delete</button>
+//   );
+// };
+
+// export default DeleteButton;
+
+// ================
+
+//import React, { useState } from 'react';
+//import DeleteButton from './DeleteButton';
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const handleDeleteClick = () => {
+//     setCount(count - 1);
+//   };
+
+//   return (
+//     <div>
+//       <p>Count: {count}</p>
+//       <DeleteButton onClick={handleDeleteClick} />
+//     </div>
+//   );
+// };
+
+// export default App;
