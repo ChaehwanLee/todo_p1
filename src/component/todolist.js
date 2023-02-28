f03f179245b101d889fda722851784b134d7ed48
Chaehwan <oasisinyourmind@gmail.com>
2023-03-01 02:09:55 +0900

    remove other way

--- src/component/todolist.js
+++ src/component/todolist.js
@@ -1,6 +1,3 @@
-// import React, { useState } from "react";
-// import Todoitem from './todoitem';
-
 import React from 'react';
 
 const Todo = ({ todo, onRemove }) => {
@@ -24,29 +21,3 @@ const TodoList = ({ todos, onRemove }) => {
 };
 
 export default TodoList;
-
-
-
-// function Todolist(){
-//     const [count, setCount] = useState(0);
-//     const [todos, setTodos] = useState({
-//         id: "",
-//         text: "",
-//         fin: ""
-//     });
-//     return (
-//         <div>
-//             <p>{count}번 만들었습니다</p>
-//             <button onClick={()=> setCount(count+1)}> 누르시오 </button>
-//             {todos.map(todo =>(
-//                 <Todoitem
-//                     id={todo.id}
-//                     text={todo.text}
-//                     fin={todo.fin}
-//                 />
-//             ))}
-//         </div>
-//     );
-// }
-
-// export default Todolist;
