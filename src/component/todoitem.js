3a94a5207361bd4cff256c94c514041a0610f5d8
Chaehwan <oasisinyourmind@gmail.com>
2023-02-28 20:34:21 +0900

    delete button 2

--- src/component/todoitem.js
+++ src/component/todoitem.js
@@ -1,59 +1,23 @@
 import React from 'react';
 
-const DeleteButton = (props) => {
+const CreateUser = ({ title, work, onChange, onCreate }) => {
   return (
-    <button onClick={props.onClick}>Delete</button>
+    <div>
+      <input
+        name="title"
+        placeholder="할일"
+        onChange={onChange}
+        value={title}
+      />
+      <input
+        name="work"
+        placeholder="내용"
+        onChange={onChange}
+        value={work}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
   );
 };
 
-export default DeleteButton;
-
-{/* <button onClick={() => {
-    console.log(id)
-    if (window.confirm('${id}의 할 일 을 삭제하시겠습니까')) {
-        onDelete(id);
-    }
-
-}}
-> 삭제하시겠습니까
-
-</button> */}
-
-
-
-// function Todoitem({id, fin, text}){
-//     const [id, setId]
-// }
-
-
-// import React from 'react';
-
-// const DeleteButton = (props) => {
-//   return (
-//     <button onClick={props.onClick}>Delete</button>
-//   );
-// };
-
-// export default DeleteButton;
-
-// ================
-
-//import React, { useState } from 'react';
-//import DeleteButton from './DeleteButton';
-
-// const App = () => {
-//   const [count, setCount] = useState(0);
-
-//   const handleDeleteClick = () => {
-//     setCount(count - 1);
-//   };
-
-//   return (
-//     <div>
-//       <p>Count: {count}</p>
-//       <DeleteButton onClick={handleDeleteClick} />
-//     </div>
-//   );
-// };
-
-// export default App;
+export default CreateUser;
