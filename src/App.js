6fd52b9a335680b63d5dfd69eb985af01c29a2f1
Chaehwan <oasisinyourmind@gmail.com>
2023-04-02 13:17:22 +0900

    show landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,101 @@
-// import React from "react";
+// // import React from "react";
+// import React, { useRef, useState } from 'react';
+// import Todo from "./component/todo";
+// import Todocreate from "./component/todoCreate";
+// import Todolist from "./component/todolist";
+// // import Itemlist from "./component/todoitem";
+// import './App.css';
+
+// const App = () => {
+//   const[inputs, setInputs] = useState({
+//     title:'',
+//     work:'',
+//   });
+//   const { title, work } = inputs;
+
+//   const onChange =(e) => {
+//     const {title, value} = e.target;
+//     setInputs((lasttodos)=>({
+//       ...lasttodos,
+//       [title]: value,
+//     }));
+//   };
+
+//   const [todo, setTodo] = useState([
+//     {
+//       id: 1,
+//       title: '기상',
+//       work: '물 한 잔 마시기',
+//     },
+//     {
+//       id: 2,
+//       title: '뉴스보기',
+//       work: '경제 뉴스 읽기',
+//     },
+//     {
+//       id: 3,
+//       title: '일과시작',
+//       work: '이메일 처리하기',
+//     },
+//   ]);
+
+//   const nextId = useRef(0);
+//   const onCreate = () => {
+//     const titles ={
+//       id: nextId.current,
+//       title,
+//       work,
+//     };
+
+//   setTodo((lastTitle)=>[...lastTitle, titles]);
+//     setInputs({
+//       title: '',
+//       work: '',
+//     });
+//     nextId.current += 1;
+//   };
+
+//   const onRemove = (id) => {
+//     setTodo((lastTitle) => lastTitle.filter((item) => item.id !== id));
+//   };  
+
+//   return (
+
+//       <div>
+//       <Todo />
+//       <Todocreate
+//       title={title}
+//       work={work}
+//       onChange={onChange}
+//       onCreate={onCreate} 
+//       />
+      
+//       <Todolist todo={todo} onRemove={onRemove}/>
+
+//       </div>
+//     );
+// };
+
+// export default App;
+
+
 import React, { useRef, useState } from 'react';
 import Todo from "./component/todo";
 import Todocreate from "./component/todoCreate";
 import Todolist from "./component/todolist";
-// import Itemlist from "./component/todoitem";
 import './App.css';
+import TodoList from './component/todolist';
 
 const App = () => {
-  const[inputs, setInputs] = useState({
-    title:'',
-    work:'',
+  const [inputs, setInputs] = useState({
+    title: '',
+    work: '',
   });
   const { title, work } = inputs;
 
-  const onChange =(e) => {
-    const {title, value} = e.target;
-    setInputs((lasttodos)=>({
+  const onChange = (e) => {
+    const { title, value } = e.target;
+    setInputs((lasttodos) => ({
       ...lasttodos,
       [title]: value,
     }));
@@ -40,34 +120,38 @@ const App = () => {
   ]);
 
   const nextId = useRef(0);
+
   const onCreate = () => {
-    const titles ={
+    const newTodo = {
       id: nextId.current,
       title,
       work,
     };
 
-  setTodo((lastTitle)=>[...lastTitle, titles]);
+    setTodo((lastTodos) => [...lastTodos, newTodo]);
     setInputs({
       title: '',
       work: '',
     });
     nextId.current += 1;
   };
-  
-  return (
 
-      <div>
+  const onRemove = (id) => {
+    setTodo((lastTodos) => lastTodos.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div>
       <Todo />
       <Todocreate
-      title={title}
-      work={work}
-      onChange={onChange}
-      onCreate={onCreate} />
-      
-      <Todolist todo={todo} onRemove={onRemove}/>
-      </div>
-    );
+        title={title}
+        work={work}
+        onChange={onChange}
+        onCreate={onCreate} 
+      />
+      <Todolist todo={todo} onRemove={onRemove} />
+    </div>
+  );
 };
 
 export default App;
